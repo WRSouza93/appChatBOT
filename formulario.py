450159a7591cdabf4496999683ab60321ec3cafc
WRSouza93 <146442415+WRSouza93@users.noreply.github.com>
2024-10-29 11:32:04 -0300

    Removidas bibliotecas que não foram usadas

--- formulario.py
+++ formulario.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-from datetime import time
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
